4ac956a76c4a3d61d06fb5e9f4adac0f21bf6b44
Paulo <pauloaxn@gmail.com>
2012-10-24 23:06:48 -0200

    animate and tween solved

--- js/cube.js
+++ js/cube.js
@@ -1,46 +1,42 @@
 function Cube () {
-  this.init();
-
+	this.init();
 }
-
 Cube.prototype = {
+	init: function () {
+		this.timeLine = new TimelineLite();
 
-  init: function () {
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( this.renderer.domElement );
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( this.renderer.domElement );
 
-    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-    this.camera.position.z = 400;
+		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+		this.camera.position.z = 400;
 
-    this.scene = new THREE.Scene();
+		this.scene = new THREE.Scene();
 
-    var geometry = new THREE.CubeGeometry( 200, 200, 200 );
-    var material = new THREE.MeshBasicMaterial({ color: 0xCC0000 });
+		var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+		var material = new THREE.MeshBasicMaterial({ color: 0xCC0000 });
 
-    this.mesh = new THREE.Mesh( geometry, material );
-    this.scene.add( this.mesh );
+		this.mesh = new THREE.Mesh( geometry, material );
+		this.scene.add( this.mesh );
 
-    this.renderer.render( this.scene, this.camera );
+		this.renderer.render( this.scene, this.camera );
 
-    requestAnimationFrame( this.animate );
-  },
+		this.timeLine.to(this.mesh.position, 1, { x: 300, y: 170, z: -180, ease:Strong.easeInOut, onComplete: this.reverseAnim, onCompleteParams: [this] } );
 
-  animate: function () {
-    cube.mesh.rotation.x += 0.005;
-    cube.mesh.rotation.y += 0.01;
+		this.animate();
+	},
 
-    cube.renderer.render( cube.scene, cube.camera );
-    requestAnimationFrame( cube.animate );
-  }
+	animate: function () {
+		var self = this;
+		setInterval(function() {
+			self.mesh.rotation.y += 0.01;
+			self.mesh.rotation.x += 0.005;
+			self.renderer.render( self.scene, self.camera );
+		}, 1000/60);
+	},
+
+	reverseAnim: function (self) {
+		self.timeLine.reverse();
+	}
 };
-
-
-// function animate() {
-//       requestAnimationFrame( animate );
-
-//       cube.mesh.rotation.x += 0.005;
-//       cube.mesh.rotation.y += 0.01;
-
-//       cube.renderer.render( cube.scene, cube.camera );
-// }
